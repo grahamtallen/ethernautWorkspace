f434b52e43966d61b6141b8a22f1b5b987426bc6
grahamtallen <grahamtallen94@gmail.com>
2025-08-12 20:27:38 -0700

    Gatekeeperone debugging

--- contracts/GatekeeperOneHacker.sol
+++ contracts/GatekeeperOneHacker.sol
@@ -9,5 +9,21 @@ contract GatekeeperOneHacker {
         gatekeeper = GatekeeperOne(_gatekeeper);
     }
 
+    function callGatekeeper() public {
+        bytes8 gateKey = 0x0000000000000000;
+        (bool success, bytes memory returndata) = address(gatekeeper).call{gas: 8191}(abi.encodeWithSignature("enter(bytes8)", gateKey));
+        if (!success) {
+            if (returndata.length > 0) {
+                // Bubble up the original revert reason
+                assembly {
+                    let returndata_size := mload(returndata)
+                    revert(add(32, returndata), returndata_size)
+                }
+            } else {
+                revert("External call failed without reason");
+            }
+        }
+    }
+
     
 }
